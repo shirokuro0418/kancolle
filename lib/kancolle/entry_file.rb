cd99b6a4a7c6ce43c05472b99acadf0d52844566
shirokuro0418 <shirokuro0418@gmail.com>
2014-07-21 13:35:45 +0900

    jsonファイルのopenを使うときに初回に１回だけ読み込みに変更

--- lib/kancolle/entry_file.rb
+++ lib/kancolle/entry_file.rb
@@ -16,11 +16,13 @@ module Kancolle
     def initialize(datas = {})
       super(datas)
 
-      @start_json           = ""
-      @start2_json          = ""
-      @port_json            = ""
-      @slotitem_member_json = ""
-      @file_json            = Array.new
+      # それぞれ使うときに一度だけ読み込む
+      @json_data            = Hash.new
+      @start_json           = lambda {|x=nil| return return_json(:start, x) }
+      @start2_json          = lambda {|x=nil| return return_json(:start2, x) }
+      @port_json            = lambda {|x=nil| return return_json(:port, x) }
+      @slotitem_member_json = lambda {|x=nil| return return_json(:slotitem_member, x) }
+      @file_json            = lambda {|x=nil| return return_json(:file, x) }
     end
 
     ##################################################################
@@ -29,12 +31,10 @@ module Kancolle
     public
     # マップ情報 return [マップ番号、ステージ番号]
     def map
-      json_read
       return [@start_json["api_data"]["api_maparea_id"], @start_json["api_data"]["api_mapinfo_no"]]
     end
     # レベル
     def lvs
-      json_read
       lv = Array.new
       @port_json["api_data"]["api_ship"].each do |kanmusu|
         ids.each_with_index do |id, i|
@@ -47,9 +47,8 @@ module Kancolle
     end
     # ボーキサイト
     def bauxite
-      json_read
       count_lost = 0
-      @file_json.each do |file_json|
+      @file_json.call.each do |file_json|
         next if file_json[:battle].nil?
         if file_json[:battle]['api_data']['api_stage_flag'][0] == 1
           count_lost     += file_json[:battle]['api_data']['api_kouku']["api_stage1"]["api_f_lostcount"]
@@ -62,10 +61,8 @@ module Kancolle
     end
     # 燃料
     def lost_fuels
-      json_read
-
       # 最後のship2ファイルのJSON
-      ship2 = @file_json.select {|file_json| file_json[:ship2] unless file_json[:ship2].nil?}.last[:ship2]
+      ship2 = @file_json.call.select {|file_json| file_json[:ship2] unless file_json[:ship2].nil?}.last[:ship2]
 
       now_fuel = Array.new
       max_fuel = Array.new
@@ -85,7 +82,7 @@ module Kancolle
       end
 
       count = 0
-      count += 1 unless @file_json.last[:battle].nil?
+      count += 1 unless @file_json.call.last[:battle].nil?
       max_fuel.each_with_index do |m_fuel, i|
         now_fuel[i] -= (m_fuel * (count * 0.2)).to_i
       end
@@ -97,10 +94,9 @@ module Kancolle
     end
     # ルート
     def route
-      json_read
       route = Array.new
       route.push(@start_json["api_data"]["api_no"])
-      tmp_file = @file_json.clone
+      tmp_file = @file_json.call.clone
       tmp_file.shift
       tmp_file.each do |file_json|
         route.push(file_json[:next]["api_data"]["api_no"])
@@ -109,7 +105,6 @@ module Kancolle
     end
     # 名前
     def names
-      json_read
       names        = Array.new(6).map{nil}
       id_names     = Array.new(6).map{nil}
       sortno_names = Array.new(6).map{nil}
@@ -128,9 +123,8 @@ module Kancolle
     end
     # 勝利判定
     def hantei
-      json_read
       hantei = Array.new
-      @file_json.each_with_index do |file_json, i|
+      @file_json.call.each_with_index do |file_json, i|
         if file_json[:battle].nil?
           hantei[i] = nil
         else
@@ -142,27 +136,56 @@ module Kancolle
 
     private
     # JSONを読み込み
-    def json_read
-      open(@start) {|p| @start_json  = JSON::parse(p.read)} if @start_json.empty?
-      open(@start2){|p| @start2_json = JSON::parse(p.read)} if @start2_json.empty?
-      open(@port)  {|p| @port_json   = JSON::parse(p.read)} if @port_json.empty?
-      open(@slotitem_member){|p| @slotitem_member_json = JSON::parse(p.read)} if @slotitem_member_json.empty?
-      if @file_json.empty?
+    # def json_read
+    #   open(@start) {|p| @start_json  = JSON::parse(p.read)} if @start_json.empty?
+    #   open(@start2){|p| @start2_json = JSON::parse(p.read)} if @start2_json.empty?
+    #   open(@port)  {|p| @port_json   = JSON::parse(p.read)} if @port_json.empty?
+    #   open(@slotitem_member){|p| @slotitem_member_json = JSON::parse(p.read)} if @slotitem_member_json.empty?
+    #   if @file_json.empty?
+    #     @file.each_with_index do |mass, i|
+    #       mass_json = Hash.new
+    #       mass.each do |key, value|
+    #         unless value.nil?
+    #           open(value){|j| mass_json[key] = JSON::parse(j.read)}
+    #         else
+    #           mass_json[key] = nil
+    #         end
+    #       end
+    #       @file_json[i] = mass_json
+    #     end
+    #   end
+    # end
+    def ids
+      @port_json["api_data"]["api_deck_port"][0]["api_ship"]
+    end
+    def return_json(key, x)
+      if @json_data[key].nil? then json_read(key) end
+      if x.nil? then return @json_data[key] else return @json_data[key][x] end
+    end
+    def json_read(key)
+      case key
+      when :start
+        open(@start) {|j| @json_data[key] = JSON::parse(j.read)}
+      when :start2
+        open(@start2) {|j| @json_data[key] = JSON::parse(j.read)}
+      when :port
+        open(@port) {|j| @json_data[key] = JSON::parse(j.read)}
+      when :slotitem_member
+        open(@slotitem_member) {|j| @json_data[key] = JSON::parse(j.read)}
+      when :file
+        @json_data[key] = Array.new
         @file.each_with_index do |mass, i|
           mass_json = Hash.new
           mass.each do |key, value|
-            unless value.nil?
-              open(value){|j| mass_json[key] = JSON::parse(j.read)}
-            else
+            if value.nil?
               mass_json[key] = nil
+            else
+              open(value){|j| mass_json[key] = JSON::parse(j.read)}
             end
           end
-          @file_json[i] = mass_json
+          @json_data[key][i] = mass_json
         end
       end
-    end
-    def ids
-      @port_json["api_data"]["api_deck_port"][0]["api_ship"]
     end
 
     ##################################################################
