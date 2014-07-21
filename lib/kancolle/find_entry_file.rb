c387198646281ff548def78b73020ff4d86c65ed
shirokuro0418 <shirokuro0418@gmail.com>
2014-07-21 13:53:50 +0900

    FindEntryFile:end_port end_slotitem_memberの追加

--- lib/kancolle/find_entry_file.rb
+++ lib/kancolle/find_entry_file.rb
@@ -25,33 +25,55 @@ module Kancolle
         battle_file            = nil
         battle_midnight_file   = nil
         syutugeki_arr          = nil
+        end_port_flg           = false
+        end_slotitem_flg       = false
         e_start_file           = nil
         e_start2_file          = nil
         e_slotitem_member_file = nil
         e_port_file            = nil
+        e_end_port_file          = nil # 出撃終了時
+        e_end_slotitem_member_file = nil
 
         dir.sort.each do |file|
           start2_file          = dir.path.sub(/[^\/]$/, '\&/') + file if file =~ /_START2.json$/ # start2ファイルの抽出
-          port_file            = dir.path.sub(/[^\/]$/, '\&/') + file if file =~ /_PORT.json$/     # portファイルの輸出
-          # SLOTITEM_MEMBERの輸出
-          slotitem_member_file = dir.path.sub(/[^\/]$/, '\&/') + file if file =~ /_SLOTITEM_MEMBER.json$/
+          if file =~ /_PORT.json$/
+            port_file            = dir.path.sub(/[^\/]$/, '\&/') + file  # portファイルの輸出
+            if end_port_flg
+              e_end_port_file = port_file
+              end_port_flg = false
+            end
+          end
+          if file =~ /_SLOTITEM_MEMBER.json$/
+            slotitem_member_file = dir.path.sub(/[^\/]$/, '\&/') + file # SLOTITEM_MEMBERの輸出
+            if end_slotitem_flg
+              e_end_slotitem_member_file = slotitem_member
+              end_slotitem_flg = false
+            end
+          end
           ship2_file           = dir.path.sub(/[^\/]$/, '\&/') + file if file =~ /_SHIP2.json$/
           battle_file          = dir.path.sub(/[^\/]$/, '\&/') + file if file =~ /_BATTLE.json$/
           battle_midnight_file = dir.path.sub(/[^\/]$/, '\&/') + file if file =~ /_BATTLE_MIDNIGHT.json$/
 
           if file =~ /_START.json$/
             unless syutugeki_arr.nil?
-              entryfiles.push(EntryFile.new({ "start"           => e_start_file,
-                                              "file"            => syutugeki_arr,
-                                              "start2"          => e_start2_file,
-                                              "slotitem_member" => e_slotitem_member_file,
-                                              "port"            => e_port_file }))
+              entryfiles.push(EntryFile.new({ "start"                 => e_start_file,
+                                              "file"                  => syutugeki_arr,
+                                              "start2"                => e_start2_file,
+                                              "slotitem_member"       => e_slotitem_member_file,
+                                              "port"                  => e_port_file,
+                                              "end_port"              => e_end_port_file,
+                                              "e_end_slotitem_member" => e_end_slotitem_member_file
+                                            }))
             end
-            e_start_file           = dir.path.sub(/[^\/]$/, '\&/') + file
-            e_start2_file          = start2_file
-            e_slotitem_member_file = slotitem_member_file
-            e_port_file            = port_file
-            syutugeki_arr          = Array.new
+            e_start_file               = dir.path.sub(/[^\/]$/, '\&/') + file
+            end_port_flg               = true
+            end_slotitem_flg           = true
+            e_start2_file              = start2_file
+            e_slotitem_member_file     = slotitem_member_file
+            e_port_file                = port_file
+            e_end_port_file            = nil
+            e_end_slotitem_member_file = nil
+            syutugeki_arr              = Array.new
           end
 
           # nextファイルからステージ情報の輸出
@@ -81,11 +103,14 @@ module Kancolle
           end
         end
         unless syutugeki_arr.nil?
-          entryfiles.push(EntryFile.new({ "start"           => e_start_file,
-                                          "file"            => syutugeki_arr,
-                                          "start2"          => e_start2_file,
-                                          "slotitem_member" => e_slotitem_member_file,
-                                          "port"            => e_port_file }))
+          entryfiles.push(EntryFile.new({ "start"                 => e_start_file,
+                                          "file"                  => syutugeki_arr,
+                                          "start2"                => e_start2_file,
+                                          "slotitem_member"       => e_slotitem_member_file,
+                                          "port"                  => e_port_file,
+                                          "end_port"              => e_end_port_file,
+                                          "e_end_slotitem_member" => e_end_slotitem_member_file
+                                        }))
         end
       end
       return EntryFiles.new(entryfiles)
