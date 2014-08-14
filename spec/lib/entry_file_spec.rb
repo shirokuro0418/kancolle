66fce4e4d4896edfb0992e25afa6ad13f53e27c8
shirokuro0418 <shirokuro0418@gmail.com>
2014-08-14 16:36:08 +0900

    テストファイル追加

--- spec/lib/entry_file_spec.rb
+++ spec/lib/entry_file_spec.rb
@@ -8,149 +8,215 @@ include Kancolle
 dir = "/Users/shirokuro11/program/kancolle/test_files/json"
 
 describe EntryFile do
-  before(:all) { @entry_file = FindEntryFile::parse_for_dir(dir).entry_files[0] }
+  before(:all){
+    ### 通常海域のjsonデータ
+    @start2              = "#{dir}/2014-07-19_221912.500_START2.json"
+    @slotitem_member     = "#{dir}/2014-07-20_165402.897_SLOTITEM_MEMBER.json"
+    @port                = "#{dir}/2014-07-20_165455.961_PORT.json"
+    @start               = "#{dir}/2014-07-20_165501.969_START.json"
+    @end_slotitem_member = "#{dir}/2014-07-20_170022.214_SLOTITEM_MEMBER.json"
+    @end_port            = "#{dir}/2014-07-20_170023.216_PORT.json"
+    @file = Array.new
+    h = Hash.new
+    h[:battle]        = "#{dir}/2014-07-20_165517.978_BATTLE.json"
+    h[:battle_result] = "#{dir}/2014-07-20_165605.007_BATTLE_RESULT.json"
+    h[:ship2]         = "#{dir}/2014-07-20_165619.016_SHIP2.json"
+    @file[0] = h
+    h = Hash.new
+    h[:next] = "#{dir}/2014-07-20_165619.020_NEXT.json"
+    @file[1] = h
+    h = Hash.new
+    h[:next]          = "#{dir}/2014-07-20_165632.028_NEXT.json"
+    h[:battle]        = "#{dir}/2014-07-20_165641.034_BATTLE.json"
+    h[:battle_result] = "#{dir}/2014-07-20_165750.073_BATTLE_RESULT.json"
+    h[:ship2]         = "#{dir}/2014-07-20_165804.085_SHIP2.json"
+    @file[2] = h
+    h = Hash.new
+    h[:next] = "#{dir}/2014-07-20_165804.090_NEXT.json"
+    @file[3] = h
+    h = Hash.new
+    h[:next]            = "#{dir}/2014-07-20_165815.096_NEXT.json"
+    h[:battle]          = "#{dir}/2014-07-20_165827.106_BATTLE.json"
+    h[:battle_midnight] = "#{dir}/2014-07-20_165945.151_BATTLE_MIDNIGHT.json"
+    h[:battle_result]   = "#{dir}/2014-07-20_170002.160_BATTLE_RESULT.json"
+    file[4] = h
+    @datas_defult = {
+      :start2              => start2,
+      :slotitem_member     => slotitem_member,
+      :port                => port,
+      :start               => start,
+      :ent_slotitem_member => end_slotitem_member,
+      :end_port            => end_port,
+      :file                => nil
+    }
+    ###### ここまで ########
+
+    ###### 夏イベの時のjsonデータ
+    start2              = "#{dir}/2014-08-14_130735.526_START2.json"
+    slotitem_member     = "#{dir}/2014-08-14_134425.318_SLOTITEM_MEMBER.json"
+    port                = "#{dir}/2014-08-14_143336.024_PORT.json"
+    start               = "#{dir}/2014-08-14_143346.033_START.json"
+    end_slotitem_member = "#{dir}/2014-08-14_144105.384_SLOTITEM_MEMBER.json"
+    end_port            = "#{dir}/2014-08-14_144106.386_PORT.json"
+    @json_summer = {
+      :start2              => start2,
+      :slotitem_member     => slotitem_member,
+      :port                => port,
+      :start               => start,
+      :ent_slotitem_member => end_slotitem_member,
+      :end_port            => end_port,
+      :file                => nil
+    }
+  }
   # ファイルの検査
   describe 'インスタンス変数' do
-    it '.start is 2014-07-20_165501.969_START.json' do
-      expect(@entry_file.start).to eq  dir + "/2014-07-20_165501.969_START.json"
-    end
-    it ".start2 is 2014-07-19_221912.500_START2.json" do
-      expect(@entry_file.start2).to eq  dir + "/2014-07-19_221912.500_START2.json"
-    end
-    it ".slotitem_member is 2014-07-20_165402.897_SLOTITEM_MEMBER.json" do
-      expect(@entry_file.slotitem_member).to eq  dir + "/2014-07-20_165402.897_SLOTITEM_MEMBER.json"
-    end
-    it ".port is 2014-07-20_165455.961_PORT.json" do
-      expect(@entry_file.port).to eq  dir + "/2014-07-20_165455.961_PORT.json"
-    end
-    it ".end_port is 2014-07-20_170023.216_PORT.json" do
-      expect(@entry_file.end_port).to eq  dir + "/2014-07-20_170023.216_PORT.json"
-    end
-    it ".end_slotitem_member is 2014-07-20_170022.214_SLOTITEM_MEMBER.json" do
-      expect(@entry_file.end_slotitem_member).to eq  dir + "/2014-07-20_170022.214_SLOTITEM_MEMBER.json"
+    describe '通常ステージ' do
+      subject(:defult){EntryFile.new(@datas_defult)}
+      it ".start is #{@datas_defult[:start]}" do
+        expect(:defult.start).to eq  @datas_defult[:start]
+      end
     end
   end
+  #   it '.start' do
+      
+  #     expect(@entry_file.start).to eq  dir + "/2014-07-20_165501.969_START.json"
+  #   end
+  #   it ".start2 is 2014-07-19_221912.500_START2.json" do
+  #     expect(@entry_file.start2).to eq  dir + "/2014-07-19_221912.500_START2.json"
+  #   end
+  #   it ".slotitem_member is 2014-07-20_165402.897_SLOTITEM_MEMBER.json" do
+  #     expect(@entry_file.slotitem_member).to eq  dir + "/2014-07-20_165402.897_SLOTITEM_MEMBER.json"
+  #   end
+  #   it ".port is 2014-07-20_165455.961_PORT.json" do
+  #     expect(@entry_file.port).to eq  dir + "/2014-07-20_165455.961_PORT.json"
+  #   end
+  #   it ".end_port is 2014-07-20_170023.216_PORT.json" do
+  #     expect(@entry_file.end_port).to eq  dir + "/2014-07-20_170023.216_PORT.json"
+  #   end
+  #   it ".end_slotitem_member is 2014-07-20_170022.214_SLOTITEM_MEMBER.json" do
+  #     expect(@entry_file.end_slotitem_member).to eq  dir + "/2014-07-20_170022.214_SLOTITEM_MEMBER.json"
+  #   end
+  # end
 
 
-  # メソッド
-  describe '.map' do
-    subject(:map) { @entry_file.map }
-    it { expect(map).to eq([5,4]) }
-  end
-  describe '.lvs' do
-    subject(:lvs) { @entry_file.lvs }
-    it "has [39,82,96,87,135,80]" do
-      expect(lvs).to eq [39,82,96,87,135,80]
-    end
-  end
-  describe '.lost_bauxites' do
-    let(:bauxites) { @entry_file.lost_bauxites }
-    it "has [0,0,5,0,40,55]" do
-      expect(bauxites).to eq [0, 0, 5, 0, 40, 55]
-    end
-    it "sum 100 for the lost bauxites" do
-      expect(bauxites.inject(:+)).to eq  100
-    end
-  end
-  describe '.lost_fuels' do
-    let(:lost_fuels) { @entry_file.lost_fuels }
-    it "has [35,75,39,35,60,51]" do
-      expect(lost_fuels).to eq [35, 75, 39, 35, 60, 51]
-    end
-    it "sum 295 for lost_fuels" do
-      expect(lost_fuels.inject(:+)).to eq 295
-    end
-  end
-  describe '.lost_bulls' do
-    let(:lost_bulls) {@entry_file.lost_bulls}
-    it 'has [18,74,46,53,45,49]' do
-      expect(lost_bulls).to eq [18, 74, 46, 53, 45, 49]
-    end
-    it "sum 285 for lost_bulls" do
-      expect(lost_bulls.inject(:+)).to eq 285
-    end
-  end
-  describe '.routes' do
-    let(:route) {@entry_file.route}
+  # # メソッド
+  # describe '.map' do
+  #   subject(:map) { @entry_file.map }
+  #   it { expect(map).to eq([5,4]) }
+  # end
+  # describe '.lvs' do
+  #   subject(:lvs) { @entry_file.lvs }
+  #   it "has [39,82,96,87,135,80]" do
+  #     expect(lvs).to eq [39,82,96,87,135,80]
+  #   end
+  # end
+  # describe '.lost_bauxites' do
+  #   let(:bauxites) { @entry_file.lost_bauxites }
+  #   it "has [0,0,5,0,40,55]" do
+  #     expect(bauxites).to eq [0, 0, 5, 0, 40, 55]
+  #   end
+  #   it "sum 100 for the lost bauxites" do
+  #     expect(bauxites.inject(:+)).to eq  100
+  #   end
+  # end
+  # describe '.lost_fuels' do
+  #   let(:lost_fuels) { @entry_file.lost_fuels }
+  #   it "has [35,75,39,35,60,51]" do
+  #     expect(lost_fuels).to eq [35, 75, 39, 35, 60, 51]
+  #   end
+  #   it "sum 295 for lost_fuels" do
+  #     expect(lost_fuels.inject(:+)).to eq 295
+  #   end
+  # end
+  # describe '.lost_bulls' do
+  #   let(:lost_bulls) {@entry_file.lost_bulls}
+  #   it 'has [18,74,46,53,45,49]' do
+  #     expect(lost_bulls).to eq [18, 74, 46, 53, 45, 49]
+  #   end
+  #   it "sum 285 for lost_bulls" do
+  #     expect(lost_bulls.inject(:+)).to eq 285
+  #   end
+  # end
+  # describe '.routes' do
+  #   let(:route) {@entry_file.route}
 
-    it "is [1,8,17,18,19] for " do
-      expect(route).to eq [1, 8, 17, 18, 19]
-    end
-  end
-  describe '.names' do
-    let(:names) {@entry_file.names}
+  #   it "is [1,8,17,18,19] for " do
+  #     expect(route).to eq [1, 8, 17, 18, 19]
+  #   end
+  # end
+  # describe '.names' do
+  #   let(:names) {@entry_file.names}
 
-    it "has ['あきつ丸改','扶桑改','利根改二','羽黒改二','大鳳改','瑞鶴改']" do
-      expect(names).to eq ['あきつ丸改','扶桑改','利根改二','羽黒改二','大鳳改','瑞鶴改']
-    end
-  end
-  describe '.hantei' do
-    let(:hantei) {@entry_file.hantei}
+  #   it "has ['あきつ丸改','扶桑改','利根改二','羽黒改二','大鳳改','瑞鶴改']" do
+  #     expect(names).to eq ['あきつ丸改','扶桑改','利根改二','羽黒改二','大鳳改','瑞鶴改']
+  #   end
+  # end
+  # describe '.hantei' do
+  #   let(:hantei) {@entry_file.hantei}
 
-    it "has ['勝利S', nil, '勝利S', nil, '勝利S']" do
-      expect(hantei).to eq ['勝利S',nil,'勝利S', nil,'勝利S']
-    end
-  end
-  describe '.slots' do
-    subject(:slots) {@entry_file.slots}
+  #   it "has ['勝利S', nil, '勝利S', nil, '勝利S']" do
+  #     expect(hantei).to eq ['勝利S',nil,'勝利S', nil,'勝利S']
+  #   end
+  # end
+  # describe '.slots' do
+  #   subject(:slots) {@entry_file.slots}
 
-    it "はArrayである" do
-      expect(slots).to be_an_instance_of Array
-    end
-    it "は６つからなる" do
-      expect(slots.length).to eq 6
-    end
-  end
-  describe '.hougeki1' do
-    subject(:hougeki1) {@entry_file.hougeki1}
+  #   it "はArrayである" do
+  #     expect(slots).to be_an_instance_of Array
+  #   end
+  #   it "は６つからなる" do
+  #     expect(slots.length).to eq 6
+  #   end
+  # end
+  # describe '.hougeki1' do
+  #   subject(:hougeki1) {@entry_file.hougeki1}
 
-    it 'はArrayである' do
-      expect(hougeki1).to be_an_instance_of Array
-    end
-    it 'は５つからなる' do
-      expect(hougeki1.length).to eq 5
-    end
-  end
-  describe '.hougeki2' do
-    subject(:hougeki1) {@entry_file.hougeki1}
+  #   it 'はArrayである' do
+  #     expect(hougeki1).to be_an_instance_of Array
+  #   end
+  #   it 'は５つからなる' do
+  #     expect(hougeki1.length).to eq 5
+  #   end
+  # end
+  # describe '.hougeki2' do
+  #   subject(:hougeki1) {@entry_file.hougeki1}
 
-    it 'はArrayである' do
-      expect(hougeki1).to be_an_instance_of Array
-    end
-    it 'は５つからなる' do
-      expect(hougeki1.length).to eq 5
-    end
-  end
-  describe '.rengeki' do
-    subject(:rengeki) {@entry_file.rengeki}
+  #   it 'はArrayである' do
+  #     expect(hougeki1).to be_an_instance_of Array
+  #   end
+  #   it 'は５つからなる' do
+  #     expect(hougeki1.length).to eq 5
+  #   end
+  # end
+  # describe '.rengeki' do
+  #   subject(:rengeki) {@entry_file.rengeki}
 
-    it 'はArrayである' do
-      expect(rengeki).to be_an_instance_of Array
-    end
-    it 'は６つからなる' do
-      expect(rengeki.length).to eq 6
-    end
-  end
-  describe '.battle_forms' do
-    subject(:battle_forms) {@entry_file.battle_forms}
+  #   it 'はArrayである' do
+  #     expect(rengeki).to be_an_instance_of Array
+  #   end
+  #   it 'は６つからなる' do
+  #     expect(rengeki.length).to eq 6
+  #   end
+  # end
+  # describe '.battle_forms' do
+  #   subject(:battle_forms) {@entry_file.battle_forms}
 
-    it 'はArrayである' do
-      expect(battle_forms).to be_an_instance_of Array
-    end
-    it 'は５つからなる' do
-      expect(battle_forms.length).to eq 5
-    end
-  end
-  describe '.seiku' do
-    subject(:seiku) {@entry_file.seiku}
+  #   it 'はArrayである' do
+  #     expect(battle_forms).to be_an_instance_of Array
+  #   end
+  #   it 'は５つからなる' do
+  #     expect(battle_forms.length).to eq 5
+  #   end
+  # end
+  # describe '.seiku' do
+  #   subject(:seiku) {@entry_file.seiku}
 
-    it 'はArrayである' do
-      expect(seiku).to be_an_instance_of Array
-    end
-    it 'は５つからなる' do
-      expect(seiku.length).to eq 5
-    end
-  end
-
+  #   it 'はArrayである' do
+  #     expect(seiku).to be_an_instance_of Array
+  #   end
+  #   it 'は５つからなる' do
+  #     expect(seiku.length).to eq 5
+  #   end
+  # end
 
 end
--- spec/lib/rengeki_spec.rb
+++ spec/lib/rengeki_spec.rb
@@ -3,8 +3,11 @@ require "spec_helper"
 require "./lib/kancolle"
 include Kancolle
 
+dir = "/Users/shirokuro11/program/kancolle/test_files/json"
+
 describe Rengeki do
   before(:all){
+    ### 通常海域のjsonデータ
     start2              = nil
     slotitem_member     = nil
     port                = nil
@@ -41,7 +44,7 @@ describe Rengeki do
     open("#{dir}/2014-07-20_165945.151_BATTLE_MIDNIGHT.json"){|f| h[:battle_midnight] = JSON::parse(f.read)}
     open("#{dir}/2014-07-20_170002.160_BATTLE_RESULT.json"){|f| h[:battle_result] = JSON::parse(f.read)}
     file[4] = h
-    @json = {
+    @json_defult = {
       :start2              => start2,
       :slotitem_member     => slotitem_member,
       :port                => port,
@@ -50,14 +53,44 @@ describe Rengeki do
       :end_port            => end_port,
       :file                => file
     }
+    ###### ここまで ########
+
+    ###### 夏イベの時のjsonデータ
+    start2              = nil
+    slotitem_member     = nil
+    port                = nil
+    start               = nil
+    end_slotitem_member = nil
+    end_port            = nil
+    open("#{dir}/2014-08-14_130735.526_START2.json"){|f| start2 = JSON::parse(f.read)}
+    open("#{dir}/2014-08-14_134425.318_SLOTITEM_MEMBER.json"){|f| slotitem_member = JSON::parse(f.read)}
+    open("#{dir}/2014-08-14_143336.024_PORT.json"){|f| port = JSON::parse(f.read)}
+    open("#{dir}/2014-08-14_143346.033_START.json"){|f| start = JSON::parse(f.read)}
+    open("#{dir}/2014-08-14_144105.384_SLOTITEM_MEMBER.json"){|f| end_slotitem_member = JSON::parse(f.read)}
+    open("#{dir}/2014-08-14_144106.386_PORT.json"){|f| end_port = JSON::parse(f.read)}
+    @json_summer = {
+      :start2              => start2,
+      :slotitem_member     => slotitem_member,
+      :port                => port,
+      :start               => start,
+      :ent_slotitem_member => end_slotitem_member,
+      :end_port            => end_port,
+      :file                => nil
+    }
   }
-  describe '.read(json)' do
-    subject(:read){Rengeki.read(@json)}
+  describe '.read(json) at defult' do
+    subject(:read){Rengeki.read(@json_defult)}
     it 'return is [ 0,3,2,3,0,0 ]' do
       expect(read).to eq [ 0,3,2,3,0,0 ]
     end
     it 'return のlengthは6' do
       expect(read.length).to be 6
+    end
+  end
+  describe 'read(json) at 2014 summer' do
+    subject(:read){Rengeki.read(@json_summer)}
+    it 'return is nil' do
+      expect(read).to eq nil
     end
   end
 end
